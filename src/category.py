60338a9f375d342951d5e0a5cfdf6bd9c6f3de9a
PinderKingui1488 <ananimuszahar@gmail.com>
2024-09-25 14:18:01 +0300

    ya zataymil requeme

---
+++ src/category.py
@@ -0,0 +1,24 @@
+class Category:
+    name: str
+    description: str
+    products: list
+    product_count = 0
+    category_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        self.product_count = len(products)
+        Category.category_count += 1
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_list = []
+        for product in self.__products:
+            product_list.append(str(product))
+        return product_list
---
+++ tests/tests_clacces.py
@@ -0,0 +1,40 @@
+from src.category import Category
+from src.product import Product
+
+
+def test_product_creation() -> None:
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет", 180000.0, 5)
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет"
+    assert product.price == 180000.0
+    assert product.quantity == 5
+
+
+def test_price_setter_getter() -> None:
+    product = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product.price = 200000.0
+    assert product.price == 200000.0
+    product.price = -100
+    assert product.price == 200000.0
+
+
+def test_category_creation() -> None:
+    category = Category("Смартфоны", "Лучшие смартфоны 2023 года", [])
+    assert category.name == "Смартфоны"
+    assert category.description == "Лучшие смартфоны 2023 года"
+    assert len(category.products) == 0
+
+
+def test_add_product_to_category() -> None:
+    category = Category("Смартфоны", "Лучшие смартфоны 2023 года", [])
+    product = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    category.add_product(product)
+    assert len(category.products) == 1
+    assert category.products[0] == "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+
+
+if __name__ == "__main__":
+    test_product_creation()
+    test_price_setter_getter()
+    test_category_creation()
+    test_add_product_to_category()
